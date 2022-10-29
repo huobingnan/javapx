1c96c16b6c20bbce606a994e182513a436e4aa57
manan <75068951+huobingnan@users.noreply.github.com>
2022-10-29 16:38:05 +0800

    Rename package

--- src/main/java/io/bryan/jvmabi/constant/IJvmConstant.java
+++
@@ -1,9 +0,0 @@
-package io.bryan.jvmabi.constant;
-
-import io.bryan.jvmabi.reader.IByteCodeReader;
-
-public interface IJvmConstant {
-    byte getTag();
-
-    void read(IByteCodeReader reader);
-}

2c4c58685dbe6c7264efb1b7aba07c213a62e93a
BRYAN <2020309894@qq.com>
2024-06-01 17:11:01 +0800

    常量池测试代码

--- src/test/java/buddha/jvmabi/ConstantPool1Test.java
+++ src/test/java/buddha/jvmabi/ConstantPool1Test.java
@@ -1,4 +1,5 @@
 package buddha.jvmabi;
+import buddha.jvmabi.constant.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,6 +17,32 @@ public class ConstantPool1Test extends BaseTest {
         assertEquals(85, classFile.getConstantPool().length());
         assertEquals(23, classFile.getThisClass());
         assertEquals(2, classFile.getSuperClass());
+    }
+
+    @Test
+    public void shouldGetCorrectUtf8Info() {
+        final ClassFileConstantPool cp = classFile.getConstantPool();
+        assertEquals("java/lang/Object", cp.<ConstantUtf8>getExact(4).contentToString());
+        assertEquals("<init>", cp.<ConstantUtf8>getExact(5).contentToString());
+        assertEquals("()V", cp.<ConstantUtf8>getExact(6).contentToString());
+        assertEquals("run", cp.<ConstantUtf8>getExact(9).contentToString());
+        assertEquals("()Ljava/lang/Runnable;", cp.<ConstantUtf8>getExact(10).contentToString());
+    }
+
+    @Test
+    public void shouldGetCorrectIntAndFloatInfo() {
+        final ClassFileConstantPool cp = classFile.getConstantPool();
+        assertEquals(1, cp.<ConstantInteger>getExact(28).getValue());
+        assertEquals(1.2, cp.<ConstantFloat>getExact(35).getValue(), 1e-6);
+    }
+
+    @Test
+    public void shouldGetCorrectLongAndDoubleInfo() {
+        final ClassFileConstantPool cp = classFile.getConstantPool();
+        assertEquals(2, cp.<ConstantLong>getExact(31).getValue());
+        assertEquals(1.3, cp.<ConstantDouble>getExact(38).getValue(), 1e-6);
+        assertNull(cp.get(32));
+        assertNull(cp.get(39));
     }
 
     @Override

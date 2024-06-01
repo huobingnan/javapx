b0ab187438a9fb21e28e56643b9b803eaa97bd07
BRYAN <2020309894@qq.com>
2024-06-01 14:26:41 +0800

    给IByteCodeReader添加注释

--- src/main/java/buddha/jvmabi/constant/IJvmConstant.java
+++ src/main/java/buddha/jvmabi/constant/IJvmConstant.java
@@ -2,8 +2,22 @@ package buddha.jvmabi.constant;
 
 import buddha.jvmabi.reader.IByteCodeReader;
 
+/**
+ * JVM Constant常量接口
+ * @author BRYAN
+ */
 public interface IJvmConstant {
+    /**
+     * 获取常量标签
+     * @see buddha.jvmabi.ClassFileConstantTagConst
+     * @return 常量标签
+     */
     byte getTag();
 
+    /**
+     * 通过IByteCodeReader读取常量池信息
+     * @see IByteCodeReader
+     * @param reader IByteCodeReader
+     */
     void read(IByteCodeReader reader);
 }

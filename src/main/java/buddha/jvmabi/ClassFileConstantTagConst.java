04168f206c969df88c250c298ff7df97484b4db6
BRYAN <2020309894@qq.com>
2024-06-01 15:10:17 +0800

    修改U1,U2...U8的Java类型代表，并为其添加注释

--- src/main/java/buddha/jvmabi/ClassFileConstantTagConst.java
+++ src/main/java/buddha/jvmabi/ClassFileConstantTagConst.java
@@ -1,21 +1,24 @@
 package buddha.jvmabi;
 
-public class ClassFileConstantTagConst {
-    public static final byte UTF8_INFO = 1;
-    public static final byte INTEGER_INFO = 3;
-    public static final byte FLOAT_INFO = 4;
-    public static final byte LONG_INFO = 5;
-    public static final byte DOUBLE_INFO = 6;
-    public static final byte CLASS_INFO = 7;
-    public static final byte STRING_INFO = 8;
-    public static final byte FIELD_REF_INFO = 9;
-    public static final byte METHOD_REF_INFO = 10;
-    public static final byte INTERFACE_METHOD_REF_INFO = 11;
-    public static final byte NAME_AND_TYPE_INFO = 12;
-    public static final byte METHOD_HANDLE_INFO = 15;
-    public static final byte METHOD_TYPE_INFO = 16;
-    public static final byte DYNAMIC_INFO = 17;
-    public static final byte INVOKE_DYNAMIC_INFO = 18;
-    public static final byte MODULE_INFO = 19;
-    public static final byte PACKAGE_INFO = 20;
+import buddha.jvmabi.annotation.U1;
+import buddha.jvmabi.annotation.U2;
+
+public interface ClassFileConstantTagConst {
+    @U1 int UTF8_INFO                 = 1;
+    @U1 int INTEGER_INFO              = 3;
+    @U1 int FLOAT_INFO                = 4;
+    @U1 int LONG_INFO                 = 5;
+    @U1 int DOUBLE_INFO               = 6;
+    @U1 int CLASS_INFO                = 7;
+    @U1 int STRING_INFO               = 8;
+    @U1 int FIELD_REF_INFO            = 9;
+    @U1 int  METHOD_REF_INFO          = 10;
+    @U1 int INTERFACE_METHOD_REF_INFO = 11;
+    @U1 int NAME_AND_TYPE_INFO        = 12;
+    @U1 int METHOD_HANDLE_INFO        = 15;
+    @U1 int METHOD_TYPE_INFO          = 16;
+    @U1 int DYNAMIC_INFO              = 17;
+    @U1 int INVOKE_DYNAMIC_INFO       = 18;
+    @U1 int MODULE_INFO               = 19;
+    @U1 int PACKAGE_INFO              = 20;
 }
